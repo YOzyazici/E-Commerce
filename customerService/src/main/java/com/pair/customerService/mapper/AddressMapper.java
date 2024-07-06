52e87ee13d7e48f698546efdfe21312fecc02366
omer-ylmz <omer.yilmaz.gru@hotmail.com>
2024-07-06 14:15:13 +0300

    mapstruct fixed

--- customerService/src/main/java/com/pair/customerService/mapper/AddressMapper.java
+++ customerService/src/main/java/com/pair/customerService/mapper/AddressMapper.java
@@ -5,7 +5,7 @@ import com.pair.customerService.entities.concretes.Address;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AddressMapper {
     AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
 
